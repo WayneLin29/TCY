1608f9174210b0f7595b92cc32a6b522679930f5
林子瑋 <JO3.JO3@hotmail.com>
2022-12-02 10:45:04 +0800

    1.Process Order 新增Create receipt

---
+++ LUMTCYCustomization/LUMTCYCustomization/Graph_Extension/SOCreatShipmentExtension.cs
@@ -0,0 +1,49 @@
+﻿using PX.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PX.Objects.SO
+{
+    public abstract class SOCreatShipment_ProtectedExt : PXGraphExtension<SOCreateShipment>
+    {
+        [PXProtectedAccess(typeof(SOCreateShipment))]
+        public abstract PXSelectBase<SOOrder> BuildCommandDefault();
+    }
+
+    public class SOCreatShipmentExtension : PXGraphExtension<SOCreatShipment_ProtectedExt, SOCreateShipment>
+    {
+        public delegate PXSelectBase<SOOrder> GetSelectCommandDelegate(SOOrderFilter filter);
+        [PXOverride]
+        public virtual PXSelectBase<SOOrder> GetSelectCommand(SOOrderFilter filter, GetSelectCommandDelegate baseMethod)
+        {
+            PXSelectBase<SOOrder> cmd;
+            switch (filter.Action)
+            {
+                case "SO301000$createShipmentReceipt":
+                    cmd = Base1.BuildCommandDefault();
+                    cmd.Join<LeftJoin<SOOrderShipment, On<SOOrder.orderType, Equal<SOOrderShipment.orderType>,
+                                  And<SOOrder.orderNbr, Equal<SOOrderShipment.orderNbr>>>>>();
+                    cmd.WhereAnd<Where<SOOrder.orderType,Equal<ROTypeAttr>,
+                        Or<SOOrder.orderType,Equal<RNTypeAttr>>>>();
+                    cmd.WhereAnd<Where<SOOrderShipment.orderNbr.IsNull>>();
+                    break;
+                default:
+                    cmd = baseMethod(filter);
+                    break;
+            }
+            return cmd;
+        }
+    }
+
+    public class ROTypeAttr : PX.Data.BQL.BqlString.Constant<ROTypeAttr>
+    {
+        public ROTypeAttr() : base("RO") { }
+    }
+    public class RNTypeAttr : PX.Data.BQL.BqlString.Constant<RNTypeAttr>
+    {
+        public RNTypeAttr() : base("RN") { }
+    }
+}
